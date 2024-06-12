db5956b73dc5aec58f65d537b7157b752c7496fb
AlexVulpoiu <vulpoiu_alexandru@yahoo.com>
2024-06-13 00:34:05 +0300

    add signup for normal users

--- src/app/pages/authentication/register/register.component.ts
+++ src/app/pages/authentication/register/register.component.ts
@@ -1,26 +1,36 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
 import { Router } from '@angular/router';
+import {AuthService} from "../../../services/auth.service";
+import { UserSignupRequest } from 'src/app/dto/auth/user-signup-request';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
 })
-export class AppSideRegisterComponent {
-  constructor(private router: Router) {}
+export class AppSideRegisterComponent implements OnInit {
+  signUpForm: FormGroup = new FormGroup({});
 
-  form = new FormGroup({
-    uname: new FormControl('', [Validators.required, Validators.minLength(6)]),
-    email: new FormControl('', [Validators.required]),
-    password: new FormControl('', [Validators.required]),
-  });
+  constructor(private router: Router, private fb: FormBuilder, private authService: AuthService) {}
 
-  get f() {
-    return this.form.controls;
+  ngOnInit(): void {
+    this.signUpForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      firstName: ['', [Validators.required, Validators.pattern('^[a-zA-Z\\s-]+$')]],
+      lastName: ['', [Validators.required, Validators.pattern('^[a-zA-Z\\s-]+$')]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$')
+      ]]
+    });
   }
 
-  submit() {
-    // console.log(this.form.value);
-    this.router.navigate(['/dashboard']);
+  onSubmit(): void {
+    if (this.signUpForm.valid) {
+      let form = this.signUpForm.value;
+      this.authService.registerUser(new UserSignupRequest(form.email, form.firstName, form.lastName, form.password))
+        .subscribe(() => this.router.navigate(['/authentication/signin']));
+    }
   }
 }
